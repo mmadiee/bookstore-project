eda7e079f836c8f30409bcad0470dcd6e4c32d4f
mmadiee <149312891+mmadiee@users.noreply.github.com>
2024-10-25 13:43:11 +0800

    Updated for deployment

--- backend/config.js
+++
@@ -1,3 +0,0 @@
-export const PORT = 5555;
-
-export const mongoDBURL = 'mongodb+srv://mmadiee:Ivymadi_02@bookstore-mern.1gqkv.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Bookstore-MERN'
--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 import express from "express"
-import { PORT, mongoDBURL } from "./config.js"
+import * as dotenv from 'dotenv';
+dotenv.config();
 import mongoose from "mongoose"
 import booksRoute from "./routes/booksRoute.js"
 import cors from 'cors'
@@ -12,6 +13,10 @@ app.use(express.json());
 //Middleware for handling CORS POLICY
 app.use(cors())
 
+//ENV VARIABLES
+const port = process.env.PORT 
+const mongoDbUrl = process.env.MONGODBURL
+
 app.get('/', (req, res) => {
     console.log(req)
     return res.status(234).send('Welcome to MERN stack tutorial')
@@ -21,11 +26,11 @@ app.use('/books', booksRoute)
 
 
 mongoose
-    .connect(mongoDBURL)
+    .connect(mongoDbUrl)
     .then(() => {
         console.log("App connected to the database")
-        app.listen(PORT, () => {
-            console.log(`App is listening to port: ${PORT}`)
+        app.listen(port, () => {
+            console.log(`App is listening to port: ${port}`)
         })
     })
     .catch((error) => {
